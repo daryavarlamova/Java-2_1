1f90e6e1b5ee3c3818935918bf381f0de6c7b093
Darya Varlamova <d.varlamova@icloud.com>
2021-11-30 23:09:51 +0300

    new commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 199;
+        int mile = 20;
+        int total_miles = ticket / mile;
+        System.out.println(total_miles);
+    }
+}
